f8c09f6a82a29abb424ded7ee126737376bb15bf
James Pritchard <james.edward.pritchard@gmail.com>
2022-07-09 18:32:01 -0400

    setup database

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Wrap Mongoose around local connection to MongoDB
+mongoose.connect(process.env.MONGO_CREDENTIALS, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+// Export connection 
+module.exports = mongoose.connection;
